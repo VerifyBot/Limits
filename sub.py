5b6998534c73494d72b9e4f93dcc733968861ef1
VerifyBot <nirchuk.yona@gmail.com>
2020-10-23 18:40:33 +0300

    hi

--- sub.py
+++ sub.py
@@ -1,5 +1,8 @@
 import os
 
-txt = "subd/"+"/".join([*map(str, [*range(500)])])
+txt = "subd/"+"/".join([*map(str, [*range(500)])])+"/main.html"
 os.makedirs(txt)
+with open(txt, 'w') as fp:
+    fp.write("<body>Hello World</body>")
+
 
