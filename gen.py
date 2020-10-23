2e5b1ae291494430ab556cc8b7ab1e2be5e2f5e4
VerifyBot <nirchuk.yona@gmail.com>
2020-10-23 15:17:02 +0300

    x

--- gen.py
+++ gen.py
@@ -1,4 +1,4 @@
 import string, random
-t = "\n".join([random.choice(string.printable) for _ in range(100000)])
+t = "\n".join([random.choice(string.printable) for _ in range(1000000)])
 with open('imports.py', 'w') as fp:
     fp.write(t)
