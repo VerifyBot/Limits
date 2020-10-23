b46efb6b6fb4a01d8493ac3e296e435e4304161b
VerifyBot <nirchuk.yona@gmail.com>
2020-10-23 15:12:23 +0300

    x

--- gen.py
+++ gen.py
@@ -1,3 +1,3 @@
-t = "\n".join(['from random import randint as randint; import math as myMathCommands' for _ in range(55000)])
+t = "\n".join(['from random import randint as randint; import math as myMathCommands' for _ in range(35000)])
 with open('imports.py', 'w') as fp:
     fp.write(t)
